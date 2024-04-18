d7ab7435236f4870e92f57b189eafa5f55642929
Ben Manley <benjamintmanley@gmail.com>
2024-04-18 13:53:44 -0500

    implement comment deletion

--- controllers/posts.js
+++ controllers/posts.js
@@ -21,16 +21,17 @@ function create(req, res) {
   })
 }
 
-// function show(req, res) {
-//   Profile.find(req.params.profileId)
-//   .then(profile => {
-//     Post.find({author: profile._id})
-//     .populate('author')
-//     .then(posts => {
-
-//     })
-//   })
-// }
+function show(req, res) {
+  Post.findById(req.params.postId)
+  .populate('author')
+  .populate('comments.author')
+  .then(post => {
+    res.render('posts/show' ,{
+      title: 'Post Details',
+      post
+    })
+  })
+}
 
 function index(req, res) {
   Post.find({})
@@ -53,11 +54,57 @@ function deletePost(req, res) {
   .then(post => {
     res.redirect('/posts')
   })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function addComment(req, res) {
+  Post.findById(req.params.postId)
+  .then(post => {
+    req.body.author = req.user.profile._id
+    post.comments.push(req.body)
+    post.save()
+    .then(()=> {
+      res.redirect(`/posts/${post._id}`)
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/')
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function deleteComment(req, res) {
+  Post.findById(req.params.postId)
+  .then(post => {
+    post.comments.remove({_id: req.params.commentId})
+    post.save()
+    .then(() => {
+      res.redirect(`/posts/${post._id}`)
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/')
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
 }
 
 export {
   newPost as new,
   create,
   index,
-  deletePost as delete
+  deletePost as delete,
+  show,
+  addComment,
+  deleteComment
 }
--- routes/posts.js
+++ routes/posts.js
@@ -6,8 +6,11 @@ const router = Router()
 
 router.get('/', isLoggedIn, postsCtrl.index)
 router.get('/new', isLoggedIn, postsCtrl.new)
+router.get('/:postId', isLoggedIn, postsCtrl.show)
 router.post('/', isLoggedIn, postsCtrl.create)
+router.post('/:postId/comments', isLoggedIn, postsCtrl.addComment)
 router.delete('/:postId', isLoggedIn, postsCtrl.delete)
+router.delete('/:postId/comments/:commentId', isLoggedIn, postsCtrl.deleteComment)
 
 export {
   router
